235b581728ba0f7a0d6ab6ffd05865e29ccbf450
zhaowenzi <821120024@qq.com>
2018-08-06 23:00:42 +0800

    first commit

---
+++ Multiway.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Multiway {
+    private Multiway() {
+
+    }
+
+    private static void merge(In[] streams) {
+        int n = streams.length;
+        IndexMinPQ<String> pq = new IndexMinPQ<String>(n);
+        for (int i = 0; i < n; i++) {
+            if (!streams[i].isEmpty())
+                pq.insert(i, streams[i].readString());
+        }
+        while (!pq.isEmpty()) {
+            StdOut.print(pq.minKey() + " ");
+            int i = pq.delMin();
+            if (!streams[i].isEmpty())
+                pq.insert(i, streams[i].readString());
+        }
+        StdOut.println();
+    }
+
+    public static void main(String[] args) {
+        int n = args.length;
+        In[] streams = new In[n];
+        for (int i = 0; i < n; i++) {
+            streams[i] = new In(args[i]);
+        }
+        merge(streams);
+    }
+}
